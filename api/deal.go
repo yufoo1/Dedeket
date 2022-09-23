ddc1e0044d9cbfaae59d440783ebe273b86fc611
周振源 <20373427@buaa.edu.cn>
2022-09-23 18:22:17 +0800

    完善上传新教材的接口

--- api/deal.go
+++ api/deal.go
@@ -5,24 +5,47 @@ import (
 	"E-TexSub-backend/model"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"strconv"
 	"time"
 )
 
 func UploadNewTextbook(c *gin.Context) {
 	textbook := new(model.Textbook)
-	textbook.Name = c.PostForm("name")
+	textbook.BookName = c.PostForm("bookName")
 	textbook.Writer = c.PostForm("writer")
 	textbook.Class = c.PostForm("class")
 	textbook.Description = c.PostForm("description")
+	textbook.College = c.PostForm("college")
+	total, err := strconv.ParseInt(c.PostForm("total"), 10, 64)
+	if err != nil {
+		c.JSON(200, gin.H{
+			"status": false,
+		})
+		return
+	} else {
+		textbook.Total = total
+	}
 	textbook.CreatedAt = time.Now().Format("2006-01-02 15:04:05")
-	textbook.CreatedBy, _ = global.RedisDb.Get(c, "username").Result()
+	seller, err := global.RedisDb.Get(c, "username").Result()
+	if err != nil {
+		c.JSON(200, gin.H{
+			"status": false,
+		})
+		return
+	} else {
+		textbook.Seller = seller
+	}
 	model.InsertTextbook(textbook)
+	c.JSON(200, gin.H{
+		"status": true,
+	})
 }
 
 func GetFilteredTextBook(c *gin.Context) {
-	keyword := c.PostForm("keyword")
+	bookNameKeyword := c.PostForm("bookNameKeyword")
+	classKeyword := c.PostForm("classKeyword")
 	var textbookArr []model.Textbook
-	err := global.MysqlDb.Select(&textbookArr, "select * from textbook where name like '%"+keyword+"%'")
+	err := global.MysqlDb.Select(&textbookArr, "select * from textbook where bookName like '%"+bookNameKeyword+"%' and class like '%"+classKeyword+"%'")
 	if err != nil {
 		fmt.Println("exec failed, ", err)
 		return
--- model/Textbook.go
+++ model/Textbook.go
@@ -7,17 +7,20 @@ import (
 
 type Textbook struct {
 	Id          int    `json:"id" db:"id"`
-	Name        string `json:"name" db:"name"`
+	BookName    string `json:"bookName" db:"bookName"`
 	Writer      string `json:"writer" db:"writer"`
 	Class       string `json:"class" db:"class"`
 	Description string `json:"description" db:"description"`
 	CreatedAt   string `json:"createdAt" db:"createdAt"`
-	CreatedBy   string `json:"createdBy" db:"createdBy"`
+	Seller      string `json:"seller" db:"seller"`
+	College     string `json:"college" db:"college"`
+	Remain      int64  `json:"remain" db:"remain"`
+	Total       int64  `json:"total" db:"total"`
 }
 
 func InsertTextbook(textbook *Textbook) {
-	_, err := global.MysqlDb.Exec("insert into textbook(name, writer, class, description, createdAt, createdBy)values(?, ?, ?, ?, ?, ?)",
-		textbook.Name, textbook.Writer, textbook.Class, textbook.Description, textbook.CreatedAt, textbook.CreatedBy)
+	_, err := global.MysqlDb.Exec("insert into textbook(bookName, writer, class, description, createdAt, seller, total, remain, college)values(?, ?, ?, ?, ?, ?, ?, ?, ?)",
+		textbook.BookName, textbook.Writer, textbook.Class, textbook.Description, textbook.CreatedAt, textbook.Seller, textbook.Total, textbook.Total, textbook.College)
 	if err != nil {
 		fmt.Println("exec failed, ", err)
 		return
