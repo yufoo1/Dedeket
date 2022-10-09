c643d47ab1221f457b45ddc6414b503353145b81
周振源 <20373427@buaa.edu.cn>
2022-10-09 17:06:52 +0800

    服务器迁移

--- global/config.go
+++ global/config.go
@@ -2,13 +2,13 @@ package global
 
 var MysqlUsername = "root"
 var MysqlPassword = "20011206"
-var MysqlHost = "47.104.235.207"
+var MysqlHost = "47.104.9.156"
 var MysqlPort = 3306
-var MysqlDatabase = "e_texsub"
+var MysqlDatabase = "dedeket"
 
 var MongodbUsername = "dedeket"
 var MongodbPassword = "20011206"
-var MongodbHost = "47.104.235.207"
+var MongodbHost = "47.104.9.156"
 var MongodbPort = 27017
 var MongodbDatabase = "dedeket"
 
