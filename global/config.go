ec07881895d239e5e86886bcaf7b0d8eb9dea6f0
yufoo1 <96588156+yufoo1@users.noreply.github.com>
2023-01-03 15:07:47 +0800

    update

--- global/config.go
+++ global/config.go
@@ -1,22 +1,22 @@
 package global
 
-var MysqlUsername = "root"
-var MysqlPassword = "20011206"
-var MysqlHost = "47.104.9.156"
-var MysqlPort = 3306
-var MysqlDatabase = "dedeket"
+var MysqlUsername = ""
+var MysqlPassword = ""
+var MysqlHost = ""
+var MysqlPort = 0
+var MysqlDatabase = ""
 
-var MongodbUsername = "dedeket"
-var MongodbPassword = "20011206"
-var MongodbHost = "47.104.9.156"
-var MongodbPort = 27017
-var MongodbDatabase = "dedeket"
+var MongodbUsername = ""
+var MongodbPassword = ""
+var MongodbHost = ""
+var MongodbPort = 0
+var MongodbDatabase = ""
 
-var RedisAddr = "localhost:6379"
+var RedisAddr = ""
 var RedisPassword = ""
 var RedisDatabase = 0
 
-var OssEndpoint = "oss-cn-hangzhou.aliyuncs.com"
-var OssAccessKeyId = "LTAI5tQSv1DgUygXdiooQMdr"
-var OssAccessKeySecret = "uqdFUbgeA5Aoifkj0Xp4Nsnik4jzKO"
-var OssBucketName = "dedeket"
+var OssEndpoint = ""
+var OssAccessKeyId = ""
+var OssAccessKeySecret = ""
+var OssBucketName = ""
